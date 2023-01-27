52aa9592c4fa6b68d63c40eb17210dde0247aeb3
JMR-256 <67328346+JMR-256@users.noreply.github.com>
2023-01-27 22:36:19 +0000

    change module properties

--- functional/src/test/java/com/jmr256/movieshelf/ft/ScraperApplicationTests.java
+++ functional/src/test/java/com/jmr256/movieshelf/ft/ScraperApplicationTests.java
@@ -1,10 +1,10 @@
-package functional;
+package com.jmr256.movieshelf.ft;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FunctionalApplicationTests {
+class ScraperApplicationTests {
 
     @Test
     void contextLoads() {
