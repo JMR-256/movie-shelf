481a71b904b260f3009c9e164e82d60b84378810
JMR-256 <67328346+JMR-256@users.noreply.github.com>
2023-01-27 21:40:13 +0000

    Spring intialized project. Added module for main service and scraper

---
+++ shelf-service/src/test/java/com/jmr256/movieshelf/shelf/ShelfServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmr256.movieshelf.shelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelfServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
